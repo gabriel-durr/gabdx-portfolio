62d22746cbc6c1818dfc1ed3b6c66312d54d3407
Gabriel Dürr <gabriel.durr@outlook.com>
2022-08-04 18:42:02 -0300

    :sparkles: creating  header and nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,52 @@
-import {Heading, Text, Flex, Box} from "@chakra-ui/react";
+import {Heading, Text, Flex, Box, HStack} from "@chakra-ui/react";
+import Link from "next/link";
 import {ToggleButtonIcon} from "./ToggleButtonSwitch";
 
 export function Header() {
 	return (
-		<Flex as="header" width="100%">
+		<Box
+			as="header"
+			width="100%"
+			position="fixed"
+			backdropFilter="blur(10px)"
+			zIndex="2">
 			<Flex
-				m="0.2rem auto"
-				py="0.2rem"
-				w="container.lg"
+				as="nav"
+				p="1"
+				width="100%"
+				maxW="container.md"
+				m="auto"
+				wrap="wrap"
+				align="center"
 				justify="space-between">
-				<Heading color="orangeblue.400">
-					<Text fontWeight="bold">
-						gabdr
-						<Text color="orangeblue.100" as="span">
-							.
-						</Text>
-					</Text>
-				</Heading>
-				<ToggleButtonIcon />
+				<HStack align="center">
+					<Link href="/">
+						<Heading size="lg" px="3">
+							<Text color="orangeblue.400" fontWeight="bold">
+								gabdr
+							</Text>
+						</Heading>
+					</Link>
+				</HStack>
+
+				<Flex py="0.2rem" w="auto" align="center" justify="flex-start">
+					<HStack gap="5" ml="10">
+						<Link href="#">
+							<Text fontWeight="bold">About me</Text>
+						</Link>
+
+						<Link href="#">
+							<Text fontWeight="bold">Works </Text>
+						</Link>
+						<Link href="#">
+							<Text fontWeight="bold">Contact</Text>
+						</Link>
+					</HStack>
+				</Flex>
+				<HStack ml="auto" p="3" align="right">
+					<ToggleButtonIcon />
+				</HStack>
 			</Flex>
-		</Flex>
+		</Box>
 	);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,155 +1,134 @@
 import {
 	Box,
-	Container,
 	Flex,
-	HStack,
 	Image,
 	Stack,
 	Text,
+	VStack,
+	Container,
 } from "@chakra-ui/react";
 import type {NextPage} from "next";
 
 const Home: NextPage = () => {
 	return (
-		<Container w="100vw" h="100vh">
-			<Stack spacing="100">
-				<HStack>
-					<Image src="/snoopF.jpg" />
-				</HStack>
+		<Flex as="main" h="100vh" justify="center">
+			<Flex
+				justify="center"
+				as="section"
+				w="full"
+				h="full"
+				maxW="container.sm">
+				<VStack mt="120" spacing="20">
+					<Box px="1rem">
+						<Image
+							src="/snoopF.jpg"
+							alt="Imagem do Snoop deitado"
+							borderRadius="2%"
+							width="500px"
+						/>
+					</Box>
 
-				<Flex justify="center" mt="20%">
-					<Stack spacing="4" maxW="container.sm" px="2.5">
-						<Text as="p">
-							Lorem, ipsum dolor sit amet consectetur adipisicing
-							elit. Cupiditate laboriosam necessitatibus quia
-							totam dolores! Nisi esse unde alias natus fugiat
-							asperiores provident accusantium assumenda, omnis
-							repudiandae blanditiis reiciendis odit quaerat.
-							Lorem, ipsum dolor sit amet consectetur adipisicing
-							elit. Cupiditate laboriosam necessitatibus quia
-							totam dolores! Nisi esse unde alias natus fugiat
-							asperiores provident accusantium assumenda, omnis
-							repudiandae blanditiis reiciendis odit quaerat.
+					<Stack px="4" wordBreak="break-word">
+						<Text>
+							Lorem ipsum dolor sit amet consectetur adipisicing
+							elit. Porro reiciendis, cupiditate quasi temporibus,
+							facere id autem officia quibusdam odio delectus
+							veniam perspiciatis, hic cum libero rerum maiores
+							molestias ea similique. Lorem ipsum, dolor sit amet
+							consectetur adipisicing elit. Aspernatur fuga
+							impedit cum ad vero, doloremque maiores quae,
+							aliquam temporibus cupiditate reprehenderit delectus
+							dolorum natus at optio necessitatibus pariatur
+							cumque voluptas? Lorem ipsum dolor, sit amet
+							consectetur adipisicing elit. Exercitationem omnis
+							hic recusandae non, necessitatibus facere molestias
+							veniam maxime fugiat possimus nesciunt atque harum
+							provident rerum libero alias amet minus nihil!
 						</Text>
-						<Text as="p">
-							Lorem, ipsum dolor sit amet consectetur adipisicing
-							elit. Cupiditate laboriosam necessitatibus quia
-							totam dolores! Nisi esse unde alias natus fugiat
-							asperiores provident accusantium assumenda, omnis
-							repudiandae blanditiis reiciendis odit quaerat.
-							Lorem, ipsum dolor sit amet consectetur adipisicing
-							elit. Cupiditate laboriosam necessitatibus quia
-							totam dolores! Nisi esse unde alias natus fugiat
-							asperiores provident accusantium assumenda, omnis
-							repudiandae blanditiis reiciendis odit quaerat.
+						<Text>
+							Lorem ipsum dolor sit amet consectetur adipisicing
+							elit. Porro reiciendis, cupiditate quasi temporibus,
+							facere id autem officia quibusdam odio delectus
+							veniam perspiciatis, hic cum libero rerum maiores
+							molestias ea similique. Lorem ipsum, dolor sit amet
+							consectetur adipisicing elit. Aspernatur fuga
+							impedit cum ad vero, doloremque maiores quae,
+							aliquam temporibus cupiditate reprehenderit delectus
+							dolorum natus at optio necessitatibus pariatur
+							cumque voluptas? Lorem ipsum dolor, sit amet
+							consectetur adipisicing elit. Exercitationem omnis
+							hic recusandae non, necessitatibus facere molestias
+							veniam maxime fugiat possimus nesciunt atque harum
+							provident rerum libero alias amet minus nihil!
 						</Text>
-						<Text as="p">
-							Lorem, ipsum dolor sit amet consectetur adipisicing
-							elit. Cupiditate laboriosam necessitatibus quia
-							totam dolores! Nisi esse unde alias natus fugiat
-							asperiores provident accusantium assumenda, omnis
-							repudiandae blanditiis reiciendis odit quaerat.
-							Lorem, ipsum dolor sit amet consectetur adipisicing
-							elit. Cupiditate laboriosam necessitatibus quia
-							totam dolores! Nisi esse unde alias natus fugiat
-							asperiores provident accusantium assumenda, omnis
-							repudiandae blanditiis reiciendis odit quaerat.
+						<Text>
+							Lorem ipsum dolor sit amet consectetur adipisicing
+							elit. Porro reiciendis, cupiditate quasi temporibus,
+							facere id autem officia quibusdam odio delectus
+							veniam perspiciatis, hic cum libero rerum maiores
+							molestias ea similique. Lorem ipsum, dolor sit amet
+							consectetur adipisicing elit. Aspernatur fuga
+							impedit cum ad vero, doloremque maiores quae,
+							aliquam temporibus cupiditate reprehenderit delectus
+							dolorum natus at optio necessitatibus pariatur
+							cumque voluptas? Lorem ipsum dolor, sit amet
+							consectetur adipisicing elit. Exercitationem omnis
+							hic recusandae non, necessitatibus facere molestias
+							veniam maxime fugiat possimus nesciunt atque harum
+							provident rerum libero alias amet minus nihil!
 						</Text>
-						<Text as="p">
-							Lorem, ipsum dolor sit amet consectetur adipisicing
-							elit. Cupiditate laboriosam necessitatibus quia
-							totam dolores! Nisi esse unde alias natus fugiat
-							asperiores provident accusantium assumenda, omnis
-							repudiandae blanditiis reiciendis odit quaerat.
-							Lorem, ipsum dolor sit amet consectetur adipisicing
-							elit. Cupiditate laboriosam necessitatibus quia
-							totam dolores! Nisi esse unde alias natus fugiat
-							asperiores provident accusantium assumenda, omnis
-							repudiandae blanditiis reiciendis odit quaerat.
+						<Text>
+							Lorem ipsum dolor sit amet consectetur adipisicing
+							elit. Porro reiciendis, cupiditate quasi temporibus,
+							facere id autem officia quibusdam odio delectus
+							veniam perspiciatis, hic cum libero rerum maiores
+							molestias ea similique. Lorem ipsum, dolor sit amet
+							consectetur adipisicing elit. Aspernatur fuga
+							impedit cum ad vero, doloremque maiores quae,
+							aliquam temporibus cupiditate reprehenderit delectus
+							dolorum natus at optio necessitatibus pariatur
+							cumque voluptas? Lorem ipsum dolor, sit amet
+							consectetur adipisicing elit. Exercitationem omnis
+							hic recusandae non, necessitatibus facere molestias
+							veniam maxime fugiat possimus nesciunt atque harum
+							provident rerum libero alias amet minus nihil!
 						</Text>
-						<Text as="p">
-							Lorem, ipsum dolor sit amet consectetur adipisicing
-							elit. Cupiditate laboriosam necessitatibus quia
-							totam dolores! Nisi esse unde alias natus fugiat
-							asperiores provident accusantium assumenda, omnis
-							repudiandae blanditiis reiciendis odit quaerat.
-							Lorem, ipsum dolor sit amet consectetur adipisicing
-							elit. Cupiditate laboriosam necessitatibus quia
-							totam dolores! Nisi esse unde alias natus fugiat
-							asperiores provident accusantium assumenda, omnis
-							repudiandae blanditiis reiciendis odit quaerat.
+						<Text>
+							Lorem ipsum dolor sit amet consectetur adipisicing
+							elit. Porro reiciendis, cupiditate quasi temporibus,
+							facere id autem officia quibusdam odio delectus
+							veniam perspiciatis, hic cum libero rerum maiores
+							molestias ea similique. Lorem ipsum, dolor sit amet
+							consectetur adipisicing elit. Aspernatur fuga
+							impedit cum ad vero, doloremque maiores quae,
+							aliquam temporibus cupiditate reprehenderit delectus
+							dolorum natus at optio necessitatibus pariatur
+							cumque voluptas? Lorem ipsum dolor, sit amet
+							consectetur adipisicing elit. Exercitationem omnis
+							hic recusandae non, necessitatibus facere molestias
+							veniam maxime fugiat possimus nesciunt atque harum
+							provident rerum libero alias amet minus nihil!
 						</Text>
-						<Text as="p">
-							Lorem, ipsum dolor sit amet consectetur adipisicing
-							elit. Cupiditate laboriosam necessitatibus quia
-							totam dolores! Nisi esse unde alias natus fugiat
-							asperiores provident accusantium assumenda, omnis
-							repudiandae blanditiis reiciendis odit quaerat.
-							Lorem, ipsum dolor sit amet consectetur adipisicing
-							elit. Cupiditate laboriosam necessitatibus quia
-							totam dolores! Nisi esse unde alias natus fugiat
-							asperiores provident accusantium assumenda, omnis
-							repudiandae blanditiis reiciendis odit quaerat.
-							Lorem, ipsum dolor sit amet consectetur adipisicing
-							elit. Cupiditate laboriosam necessitatibus quia
-							totam dolores! Nisi esse unde alias natus fugiat
-							asperiores provident accusantium assumenda, omnis
-							repudiandae blanditiis reiciendis odit quaerat.
-							Lorem, ipsum dolor sit amet consectetur adipisicing
-							elit. Cupiditate laboriosam necessitatibus quia
-							totam dolores! Nisi esse unde alias natus fugiat
-							asperiores provident accusantium assumenda, omnis
-							repudiandae blanditiis reiciendis odit quaerat.
-						</Text>
-
-						<Text as="p">
-							Lorem, ipsum dolor sit amet consectetur adipisicing
-							elit. Cupiditate laboriosam necessitatibus quia
-							totam dolores! Nisi esse unde alias natus fugiat
-							asperiores provident accusantium assumenda, omnis
-							repudiandae blanditiis reiciendis odit quaerat.
-							Lorem, ipsum dolor sit amet consectetur adipisicing
-							elit. Cupiditate laboriosam necessitatibus quia
-							totam dolores! Nisi esse unde alias natus fugiat
-							asperiores provident accusantium assumenda, omnis
-							repudiandae blanditiis reiciendis odit quaerat.
-							Lorem, ipsum dolor sit amet consectetur adipisicing
-							elit. Cupiditate laboriosam necessitatibus quia
-							totam dolores! Nisi esse unde alias natus fugiat
-							asperiores provident accusantium assumenda, omnis
-							repudiandae blanditiis reiciendis odit quaerat.
-							Lorem, ipsum dolor sit amet consectetur adipisicing
-							elit. Cupiditate laboriosam necessitatibus quia
-							totam dolores! Nisi esse unde alias natus fugiat
-							asperiores provident accusantium assumenda, omnis
-							repudiandae blanditiis reiciendis odit quaerat.
-						</Text>
-						<Text as="p">
-							Lorem, ipsum dolor sit amet consectetur adipisicing
-							elit. Cupiditate laboriosam necessitatibus quia
-							totam dolores! Nisi esse unde alias natus fugiat
-							asperiores provident accusantium assumenda, omnis
-							repudiandae blanditiis reiciendis odit quaerat.
-							Lorem, ipsum dolor sit amet consectetur adipisicing
-							elit. Cupiditate laboriosam necessitatibus quia
-							totam dolores! Nisi esse unde alias natus fugiat
-							asperiores provident accusantium assumenda, omnis
-							repudiandae blanditiis reiciendis odit quaerat.
-							Lorem, ipsum dolor sit amet consectetur adipisicing
-							elit. Cupiditate laboriosam necessitatibus quia
-							totam dolores! Nisi esse unde alias natus fugiat
-							asperiores provident accusantium assumenda, omnis
-							repudiandae blanditiis reiciendis odit quaerat.
-							Lorem, ipsum dolor sit amet consectetur adipisicing
-							elit. Cupiditate laboriosam necessitatibus quia
-							totam dolores! Nisi esse unde alias natus fugiat
-							asperiores provident accusantium assumenda, omnis
-							repudiandae blanditiis reiciendis odit quaerat.
+						<Text>
+							Lorem ipsum dolor sit amet consectetur adipisicing
+							elit. Porro reiciendis, cupiditate quasi temporibus,
+							facere id autem officia quibusdam odio delectus
+							veniam perspiciatis, hic cum libero rerum maiores
+							molestias ea similique. Lorem ipsum, dolor sit amet
+							consectetur adipisicing elit. Aspernatur fuga
+							impedit cum ad vero, doloremque maiores quae,
+							aliquam temporibus cupiditate reprehenderit delectus
+							dolorum natus at optio necessitatibus pariatur
+							cumque voluptas? Lorem ipsum dolor, sit amet
+							consectetur adipisicing elit. Exercitationem omnis
+							hic recusandae non, necessitatibus facere molestias
+							veniam maxime fugiat possimus nesciunt atque harum
+							provident rerum libero alias amet minus nihil!
 						</Text>
 					</Stack>
-				</Flex>
-			</Stack>
-		</Container>
+				</VStack>
+			</Flex>
+		</Flex>
 	);
 };
 
